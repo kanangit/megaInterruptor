996e670410648f8097e0cb1e7efff23cdf4afcd7
kanangit <kananovich@gmail.com>
2024-07-22 16:02:33 -0400

    up to pin 20

--- megaInterruptor/megaInterruptor.ino
+++ megaInterruptor/megaInterruptor.ino
@@ -1,6 +1,7 @@
   const int pin8 = 8; //pin 8 on the DB25 corresponds to pin 8 on Arduino Mega
   const int pin14 = 2; //pin 14 on the DB25 corresponds to pin 2 on Arduino Mega
   const int pin16 = 3; //pin 16 on the DB25 corresponds to pin 3 on Arduino Mega
+  const int pin20 = 4; //pin 20 on the DB25 corresponds to pin 4 on Arduino Mega
   
 
 // the setup function runs once when you press reset or power the board
@@ -10,6 +11,7 @@ void setup() {
   pinMode(pin8, OUTPUT);
   pinMode(pin14, OUTPUT);
   pinMode(pin16, OUTPUT);
+  pinMode(pin20, OUTPUT);
 }
 
 // the loop function runs over and over again forever
@@ -17,6 +19,7 @@ void loop() {
   digitalWrite(pin8, HIGH); // RF enable HIGH
   digitalWrite(pin14, LOW); // Blanking off
   digitalWrite(pin16, HIGH); // Burst Mode on
+  digitalWrite(pin20, HIGH); // LOAD power leveling
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(15);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
