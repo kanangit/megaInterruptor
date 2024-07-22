0a8e61dc3f8b15c07e06500fa9a20c460a171a09
kanangit <kananovich@gmail.com>
2024-07-22 15:05:22 -0400

    defined pins through pin 16

--- megaInterruptor/megaInterruptor.ino
+++ megaInterruptor/megaInterruptor.ino
@@ -1,4 +1,6 @@
   const int pin8 = 8; //pin 8 on the DB25 corresponds to pin 8 on Arduino Mega
+  const int pin14 = 2; //pin 14 on the DB25 corresponds to pin 2 on Arduino Mega
+  const int pin16 = 3; //pin 16 on the DB25 corresponds to pin 3 on Arduino Mega
   
 
 // the setup function runs once when you press reset or power the board
@@ -6,11 +8,15 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(pin8, OUTPUT);
+  pinMode(pin14, OUTPUT);
+  pinMode(pin16, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(pin8, HIGH); // RF enable HIGH
+  digitalWrite(pin14, LOW); // Blanking off
+  digitalWrite(pin16, HIGH); // Burst Mode on
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(15);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
